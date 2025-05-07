f3e2f0b9155da2da61524e01fc689614bcb711f5
IrinaTab <irina.mariya.anatoliy@gmail.com>
2025-05-07 13:07:59 +0300

    Initial commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import SwipeMenu from '../Components/SwipeMenu/SwipeMenu'
+
+const Home = () => {
+    
+    return (   
+        <>
+            <h2>HOME</h2>
+            <div>
+                <SwipeMenu />
+            </div>
+        </>  
+    );
+}
+ 
+export default Home;
