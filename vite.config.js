306915d4b6563dfb7a378530b97ab1642d0709a6
IrinaTab <irina.mariya.anatoliy@gmail.com>
2025-05-12 22:33:19 +0300

    Update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/chat-project-app/' // Если проект лежит не в корне (например, `https://user.github.io/repo/`).
 })
